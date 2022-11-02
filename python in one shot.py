7aca171902980971db8348eb8efa1ffb00413911
SPARSHA SHETTY <108998366+Sparsha07@users.noreply.github.com>
2022-11-02 12:39:17 +0530

    Add files via upload

---
+++ python in one shot.py
@@ -0,0 +1,156 @@
+#print ("hello world")
+#variables
+#name = "Sparsha"
+#age = 20
+#print (name)
+#name = "aisha"
+
+#from re import S
+#from xmlrpc.client import boolean
+
+
+#is_adult =True 
+#first_name ="Tony"
+#last_name = "Shark"
+#is_genius = True 
+#age = 51
+#print (first_name)
+#print (last_name)
+#print (is_genius)
+#name = input("what is ur name?")
+#print ("hello" + name)
+#S=  input("what is ur superhero name")
+#name = ("superhero" + S)
+#print (name)
+#old_age= input("enter your old age:")
+
+#new_age=int(old_age)+2
+#print(new_age)
+#number = 18
+#print(float(18))
+#first = input("enter fisrt number:")
+#second = input("enter second number :")
+#sum = int(first) + int(second)
+#print(sum)
+#name = "Sparsha shetty"
+#print(name.upper())
+#print(name.lower())
+#print(name.find('p'))
+##print(name.replace("Sparsha shetty", "Supriya shetty"))
+#print()
+#names = "anita nagar"
+#print ("piyu" in names)
+#arthimetic operators
+#print(5+2)
+#print(5-2)
+#print(18/9)
+#print(4//2)
+#print(5%2)
+#print(5**2)
+#i = 5
+#i= i+2
+#i +=2
+#i -=2
+#i *=2
+#print(i)
+#result = 2 + 3 * 5
+#print(result)
+#print(3>2)
+#print(3>=2)
+#print(3==3)
+#print(3!=3)
+#or operator
+#print(2>3 or 2>1)
+#and  operator
+#print(3>2 and 2>1)
+#not operator
+#print(not 2>3)
+#if esle statements
+#age = 19
+#if age>= 18:
+    #we give space for identatio in python 
+     #print("u are adult")
+     #print("you can vote")  
+#print("thankyou")
+#else if
+#if age >=18:
+    #print("you are an adult")
+    #print("you can vote")
+#elif age < 18 and age >3:
+   # print("u are in school")
+#else :
+    #print("u are a child")
+    #calculator
+
+   
+#first = input("enter first number :")
+#second = input("enter second number:")
+#first =  int(first)
+#second = int(second)
+#if  operator =="+":
+#print(first+second)
+#elif  operator =="-":
+ #print(first-second)
+#elif  operator =="*":
+ #print(first*second)
+#elif  operator =="/":
+ #print(first/second)
+#elif  operator =="%":
+ # print(first%second)
+#else:
+ #   print("invalid operation")
+    #range
+#range(5) # 0,1,2,3,4
+#num = range(5)
+#print(num)
+#loopsssss
+#i = 1
+#while i<=5:
+ #print(i* "*")
+#i = i+ 1
+#for item in range(5):
+    #print(item + 1) 
+    #list  
+#marks=[90,99,87,86,77]
+#print(marks[0:2])
+#for score in marks:
+ #   print(score)
+  #  marks.append(20)
+#marks.insert(0,99)
+#print(len(marks))
+#print(marks)
+#break n continue
+#students =["ram ","sparsha","radha"]
+#for student in students:
+ #if  student =="radha":
+ #   break; #continue same structure
+#print(student)
+#tuples
+#marks = (95,98,97)
+#print(marks.count(95))
+#print(marks.index(97))
+#set
+#marks = { 44,55,66,77,88}
+#for score in marks:
+   # print (score)
+ #dictonaries
+#marks ={"eng": 88, "chemistry" : 98,"maths":78} 
+#print(marks)
+#print(marks["chemistry"])
+#marks ["physics"] = 77;
+#print(marks)
+#FUNCTION
+#IN-BUIT 
+#MODULE
+#USER DEFINED
+#import math
+#print(dir(math))
+#from math import sqrt
+#print(sqrt(16))
+#from math import * #if u want all module use star 
+def print_sum(first,second):
+    print(first + second)
+print_sum(1,2)
+
+
+
